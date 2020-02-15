3912a6bed3939effd84fa84a665ea05edaf0e981
Howard M. Harte <hharte2@gmail.com>
2020-02-15 13:48:55 -0800

    mm_carrier: Add ability to generate CARRIER table.
    Also, dump out bit flags.

--- mm_carrier.c
+++ mm_carrier.c
@@ -54,19 +54,151 @@ const char *str_cb2[] = {
     "CB2_SPARE"
 };
 
+#define CB2_VAL (CB2_REM_CARRIER_PREFIX_ZM_LOCAL | \
+                      CB2_REM_CARRIER_PREFIX_INTRALATA | \
+                      CB2_REM_CARRIER_PREFIX_INTERLATA | \
+                      CB2_REM_CARRIER_PREFIX_INTERNATIONAL | \
+                      CB2_REM_CARRIER_PREFIX_DA | \
+                      CB2_REM_CARRIER_PREFIX_1800)
+
+carrier_table_entry_t new_carriers[] = {
+{   /* Carrier 0 */
+    .carrier_ref = 0,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C0 PIC  Inter-LATA  ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 1 */
+    .carrier_ref = 1,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C1 Coin Inter-LATA  ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 2 */
+    .carrier_ref = 2,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C2 Card Inter-LATA  ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 3 */
+    .carrier_ref = 3,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C3 PIC  Intra-LATA  ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 4 */
+    .carrier_ref = 4,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C4 Coin Intra-LATA  ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 5 */
+    .carrier_ref = 5,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C5 Card Intra-LATA  ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 6 */
+    .carrier_ref = 6,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C6 PIC  Local       ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 7 */
+    .carrier_ref = 7,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C7 Coin Local       ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 8 */
+    .carrier_ref = 8,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "C8 Card Local       ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+},
+{   /* Carrier 9 */
+    .carrier_ref = 9,
+    .carrier_num = 0x0000,
+    .valid_cards = 0x00003fff,
+    //                "                    ",
+    .display_prompt = "CARRIER 9           ",
+    .control_byte2 = CB2_VAL,
+    .control_byte  = (CB_USE_SPEC_DISPLAY_PROMPT | CB_ACCEPTS_COIN_CASH_CARDS),
+    .fgb_timer = 500,
+    .international_accept_flags = 0,
+    .call_entry = 0x00,
+}
+
+};
+
 int main(int argc, char *argv[])
 {
     FILE *instream;
+    FILE *ostream;
     int carrier_index;
     uint16_t carrier_num;
     char display_prompt_string[21];
-    uint8_t cb_flags, i;
+    uint8_t i;
 
     dlog_mt_carrier_table_t *pcarrier_table;
 
     if (argc <= 1) {
         printf("Usage:\n" \
-               "\tmm_carrier mm_table_87.bin\n");
+               "\tmm_carrier mm_table_87.bin [outputfile.bin]\n");
         return (-1);
     }
 
@@ -79,9 +211,12 @@ int main(int argc, char *argv[])
         printf("Error reading CARRIER table.\n");
         if (pcarrier_table != NULL) {
             free(pcarrier_table);
+            fclose(instream);
             return (-2);
         }
     }
+
+    fclose(instream);
 
     printf("\nDefault Carriers:\n");
     for (i = 0; i < DEFAULT_CARRIERS_MAX; i++) {
@@ -122,33 +257,31 @@ int main(int argc, char *argv[])
             pcarrier_table->carrier[carrier_index].call_entry,
             pcarrier_table->carrier[carrier_index].call_entry);
 
-        cb_flags = pcarrier_table->carrier[carrier_index].control_byte2;
-        i=0;
-        while (cb_flags != 0) {
-            if (cb_flags & 1) {
-                printf("%s ", str_cb2[i]);
-            }
-            cb_flags >>= 1;
-            i++;
-        }
+        print_bits(pcarrier_table->carrier[carrier_index].control_byte2, (char **)str_cb2);
+        print_bits(pcarrier_table->carrier[carrier_index].control_byte, (char **)str_cb);
+    }
 
-        cb_flags = pcarrier_table->carrier[carrier_index].control_byte;
-        i=0;
-        while (cb_flags != 0) {
-            if (cb_flags & 1) {
-                printf("%s ", str_cb[i]);
-            }
-            cb_flags >>= 1;
-            i++;
-        }
+    printf("\n+------------------------------------------------------------------------------------------------------+\n");
 
+    if (argc > 2) {
+        ostream = fopen(argv[2], "wb");
+    }
+
+    for (i = 0; i < DEFAULT_CARRIERS_MAX; i++) {
+        pcarrier_table->defaults[i] = i;
+    }
+    memcpy(pcarrier_table->carrier, new_carriers, sizeof(new_carriers));
+
+    /* If output file was specified, write it. */
+    if (ostream != NULL) {
+        printf("\nWriting new table to %s\n", argv[2]);
+        fwrite(pcarrier_table, sizeof(dlog_mt_carrier_table_t), 1, ostream);
+        fclose(ostream);
     }
 
     if (pcarrier_table != NULL) {
         free(pcarrier_table);
     }
-
-    printf("\n+------------------------------------------------------------------------------------------------------+\n");
 
     return (0);
 }
