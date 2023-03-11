9c27f187b84b4d81c618e8fc9b31fccb201da34a
Howard M. Harte <hharte@magicandroidapps.com>
2023-03-11 14:09:19 -0800

    mm_luhn: Generate/Check arbitrary-length numbers.
    Generate/Check arbitrary-length numbers and check positions.

--- mm_luhn.c
+++ mm_luhn.c
@@ -15,25 +15,32 @@
 
 int main(int argc, char* argv[]) {
     int luhn_check = 1;
-    size_t i = 0;
+    size_t i, card_len, check_pos = 0;
     int sum = 0;
 
     if (argc <= 1) {
         printf("Usage:\n" \
-            "\tmm_luhn <n-digit card number> - to verify the check digit.\n" \
-            "\tmm_luhn <n-digit card number> -g - to generate the check digit.\n");
+            "\tmm_luhn <n-digit card number>\n\n" \
+            "If all digits are specified, the card number is checked.\n" \
+            "If one digit is replaced with a '?', the check digit in that\n" \
+            "position will be generated.\n\n" \
+            "Examples:\n" \
+            "\tmm_luhn 4012888888881881 - check 16-digit card number.\n" \
+            "\tmm_luhn 401288888888188? - generate check digit.\n");
         return -1;
     }
 
-    if (argc > 2) {
-        luhn_check = 0;
-    }
+    card_len = strlen(argv[1]);
+    for (i = 0; i < card_len; i++) {
+        int single_digit = !(i & 1);
+        int digit_val = argv[1][card_len - i - 1];
+        int digit = digit_val - '0';
 
-    printf("%s Luhn for card number %s: ", luhn_check ? "Checking" : "Generating", argv[1]);
-
-    for (i = 0; i < strlen(argv[1]); i++) {
-        int single_digit = (i & 1);
-        int digit = argv[1][i] - '0';
+        if (digit_val == '?') {
+            luhn_check = 0;
+            check_pos = card_len - i - 1;
+            digit = 0;
+        }
 
         if (single_digit) {
             sum += digit;
@@ -52,15 +59,19 @@ int main(int argc, char* argv[]) {
 
     if (luhn_check == 0) {
         uint8_t check_digit = 10 - (sum % 10);
-        if (i % 2 == 0) check_digit /= 2;
-        printf("n=%zu, %d\n", i, check_digit);
+        if (check_digit == 10) check_digit = 0;
+
+        if (check_pos % 2 == 0) check_digit ++;
+        argv[1][check_pos] = '0' + check_digit;
+        printf("Card number: %s\n", argv[1]);
+//        printf("n=%zu, sum = %d, pos = %zu, check digit = %d\n", i, sum, check_pos, check_digit);
     }
     else {
         if (sum % 10) {
             printf("Invalid check digit: Final sum = %d, remainder=%d, check result=%d\n", sum, sum % 10, (sum % 10));
         }
         else {
-            printf("Ok\n");
+            printf("Card number %s: Ok\n", argv[1]);
         }
     }
     return (0);
